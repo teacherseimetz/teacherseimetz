ddb3fc44729c12b527e99eaa61b2d0a732201381
William Martins <wwwhmartins@gmail.com>
2025-07-26 23:20:31 -0300

    Improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,16 +5,20 @@ export function Footer() {
   return (
     <footer className={styles.container}>
       <Wrapper className={styles.wrapper}>
-        <h1 className="font-bold text-4xl lowercase">Ficou com alguma dúvida?</h1>
+        <div className="w-full flex flex-col lg:flex-row justify-between gap-8">
+          <h1 className="font-bold text-4xl lowercase">
+            Ficou com alguma dúvida?
+          </h1>
 
-        <nav className={styles.links}>
-          <a href="https://instagram.com/teacherseimetz" rel="norel noopener">
-            @teacherseimetz
-          </a>
-          <a href="mailto:teacherseimetz@gmail.com" rel="norel noopener">
-            teacherseimetz@gmail.com
-          </a>
-        </nav>
+          <nav className={styles.links}>
+            <a href="https://instagram.com/teacherseimetz" rel="norel noopener">
+              @teacherseimetz
+            </a>
+            <a href="mailto:teacherseimetz@gmail.com" rel="norel noopener">
+              teacherseimetz@gmail.com
+            </a>
+          </nav>
+        </div>
       </Wrapper>
     </footer>
   );
